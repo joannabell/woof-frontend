0a800fc65240e1e2976d5660f74162d58934eedd
Joanna Bell <jjbdigital@gmail.com>
2022-10-25 12:16:37 -0500

    work

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import './App.css';
 import NavBar from "./NavBar";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [ searchValue, setSearchValue ] = useState("")//updates on search change
+
   useEffect(() => {
     fetch("http://localhost:9292/")
       .then((r) => r.json())
       .then((data) => console.log(data))
 
   }, []);
-  
+
+  // search restaurants
+  function handleSearchChange(event) {
+    setSearchValue(event.target.value)
+  }
+
+  const searchedRestaurants = restaurants.filter((restaurant) => {
+  const restaurantName = restaurant.name.toLowerCase()
+  const search = searchValue.toLowerCase()
+
   return (
     <div className="App">
       <NavBar />
